7be81dfc978b83f0a03eb0aae91fc923efd114a6
Nolway <faizeau.alexis@gmail.com>
2022-10-25 16:46:54 +0200

    Build config

--- svelte.config.js
+++ svelte.config.js
@@ -1,5 +1,7 @@
-import adapter from '@sveltejs/adapter-auto';
+import adapter from '@sveltejs/adapter-static';
 import preprocess from 'svelte-preprocess';
+
+const dev = process.env.NODE_ENV === 'development';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
@@ -10,8 +12,13 @@ const config = {
 	kit: {
 		adapter: adapter({
 			pages: 'build',
-			assets: 'build'
-		})
+			assets: 'build',
+			fallback: 'index.html'
+		}),
+		paths: {
+			base: dev ? '' : '/repo_name'
+		},
+		appDir: 'internal'
 	}
 };
 
